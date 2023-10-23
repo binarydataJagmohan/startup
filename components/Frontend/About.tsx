1e22be380c005ce2149029c0632bdfc00ffedf59
binarydataJagmohan <info.binarydata@gmail.com>
2023-10-23 18:03:08 +0530

    startup-website-design-issue-fixed

--- components/Frontend/About.tsx
+++ components/Frontend/About.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FrequentlyQuestions from "./FrequentlyQuestions";
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 const About = () => {
   return (
     <div>
@@ -26,86 +26,110 @@ const About = () => {
         </div> */}
         {/* End Page Title Area */}
         {/* Start About Area */}
-        <section className="about-section ptb-100">
+        <section className="about-section ptb-100 pb-0">
           <div className="container-fluid">
             <div className="row">
               <div className="col-lg-6 p-0">
                 <div className="about-image">
-                  <Image src="assets/img/about/about-2.jpg" alt="image" width={1000} height={1000} 
-                  // layout="responsive"
+                  <Image
+                    src="https://img.freepik.com/free-photo/creative-designers-team-working-project-discussing-details_114579-2816.jpg?w=1380&t=st=1698054740~exp=1698055340~hmac=466d1b4da92b55444119b1f9a607282bb0640700a62e4369b36a9ed00846d569"
+                    alt="image"
+                    width={1000}
+                    height={1000}
+                    // layout="responsive"
                   />
                 </div>
               </div>
               <div className="col-lg-6">
-                <div className="about-tab" style={{"paddingTop":"0px"}}>
+                <div className="about-tab" style={{ paddingTop: "0px" }}>
                   <h2>About Our Company</h2>
                   <div className="bar" />
                   <p>
-                  At RisingCapitalist, we believe that everyone should have the opportunity to invest in startups. That's why we've created an online platform that makes it easy for investors to find and invest in startups with high growth potential.
+                    At RisingCapitalist, we believe that everyone should have
+                    the opportunity to invest in startups. That's why we've
+                    created an online platform that makes it easy for investors
+                    to find and invest in startups with high growth potential.
                   </p>
                   <p>
-                  We also believe that startups are the key to economic growth and innovation. That's why we're committed to helping startups raise the funds they need to succeed.
+                    We also believe that startups are the key to economic growth
+                    and innovation. That's why we're committed to helping
+                    startups raise the funds they need to succeed.
                   </p>
                   <p>
-                  Our mission is to empower startups and investors by providing a dynamic online platform that not only connects visionaries with capital but also equips entrepreneurs with the essential financial and legal services they need to thrive. We are dedicated to fuelling innovation, fostering growth, and making the journey of fundraising more accessible and successful for startups.
+                    Our mission is to empower startups and investors by
+                    providing a dynamic online platform that not only connects
+                    visionaries with capital but also equips entrepreneurs with
+                    the essential financial and legal services they need to
+                    thrive. We are dedicated to fuelling innovation, fostering
+                    growth, and making the journey of fundraising more
+                    accessible and successful for startups.
                   </p>
                   <div className="tab about-list-tab">
                     <ul className="tabs">
                       <li>
                         <Link href="#">Our Commitment:</Link>
                       </li>
-                      {/* <li>
-                        <Link href="#">Our Mission</Link>
-                      </li>
-                      <li>
-                        <Link href="#">Friendly Support</Link>
-                      </li> */}
                     </ul>
-                    <div className="tab_content">
-                      <div className="tabs_item">
-                        <p>
-                          <b>1.	Facilitating Connections: </b> We are committed to creating a vibrant digital ecosystem where startups can meet investors, where ideas can find support, and where dreams can become reality. We strive to bridge the gap between brilliant concepts and the resources required to bring them to life.
-                        </p>
-                        <p>
-                          <b>2.	Empowering Entrepreneurs: </b> We believe in the potential of startups to drive change. Our CFO services empower startups with financial expertise, while our legal services secure their fundraising endeavours. The addition of "Invoice Discounting" further strengthens our financial support by offering cash flow solutions that help startups maintain stability.
-                        </p>
-                        <p>
-                          <b>3.	Simplifying Fundraising: </b> We aim to simplify the fundraising process, making it more accessible and less intimidating. Our platform provides a user-friendly experience that allows startups to focus on their innovation, and investors to discover promising opportunities effortlessly.
-                        </p>
-                        <p>
-                          <b>4.	Fostering Innovation: </b> Our passion lies in nurturing innovation. We are driven to foster a community of innovators, thinkers, and doers who are shaping the future. We take pride in being an essential part of their journey.
-                        </p>
-                        <Link className="default-btn" href="#">
-                          Discover More
-                        </Link>
-                      </div>
-                      {/* <div className="tabs_item">
-                        <p>
-                          <b>Our mission</b> is to empower individuals to take control of their financial future by providing them with the knowledge, tools, and resources they need to make informed investment decisions. We believe that everyone should have access to the same opportunities and information, regardless of their financial status, and we are committed to creating a platform that levels the playing field.
-
-                          Our team of experienced professionals is dedicated to helping our users achieve their financial goals. We are constantly innovating and improving our platform to ensure that it remains the best resource for anyone who wants to invest wisely and confidently.
-                        </p>
-                        <Link className="default-btn" href="#">
-                          Discover More
-                        </Link>
-                      </div>
-                      <div className="tabs_item">
-                        <p>
-                          <b>At our startup and investment</b> website, we pride ourselves on providing friendly and helpful support to our users. Whether you have a question about your account, need help navigating the platform, or want to learn more about investing, our team is here to assist you.
-
-                          We understand that investing can be intimidating, which is why we take a personalized approach to support. Our team of experts is always available to answer your questions, provide guidance, and help you make the most of our platform. Whether you are a beginner or a seasoned investor, you can count on us to provide the support you need to succeed.
-                        </p>
-                        <Link className="default-btn" href="#">
-                          Discover More
-                        </Link>
-                      </div> */}
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </section>
+
+        {/* section startup */}
+        <section className="pb-70 pt-70 facilitating-startup">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6">
+                <div className="tab_content">
+                  <div className="tabs_item">
+                    <p>
+                      <b>1. Facilitating Connections: </b> We are committed to
+                      creating a vibrant digital ecosystem where startups can
+                      meet investors, where ideas can find support, and where
+                      dreams can become reality. We strive to bridge the gap
+                      between brilliant concepts and the resources required to
+                      bring them to life.
+                    </p>
+                    <p>
+                      <b>2. Empowering Entrepreneurs: </b> We believe in the
+                      potential of startups to drive change. Our CFO services
+                      empower startups with financial expertise, while our legal
+                      services secure their fundraising endeavours. The addition
+                      of "Invoice Discounting" further strengthens our financial
+                      support by offering cash flow solutions that help startups
+                      maintain stability.
+                    </p>
+                    <p>
+                      <b>3. Simplifying Fundraising: </b> We aim to simplify the
+                      fundraising process, making it more accessible and less
+                      intimidating. Our platform provides a user-friendly
+                      experience that allows startups to focus on their
+                      innovation, and investors to discover promising
+                      opportunities effortlessly.
+                    </p>
+                    <p>
+                      <b>4. Fostering Innovation: </b> Our passion lies in
+                      nurturing innovation. We are driven to foster a community
+                      of innovators, thinkers, and doers who are shaping the
+                      future. We take pride in being an essential part of their
+                      journey.
+                    </p>
+                    <Link className="default-btn" href="#">
+                      Discover More
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-6 textlg-end tab_content">
+                <img src="assets/images/app.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        {/* end this */}
         {/* End About Area */}
         {/* Start Team Area */}
         <section className="team-section pb-70">
@@ -132,7 +156,10 @@ const About = () => {
                       alt="image"
                     /> */}
                     <img
-                      src={process.env.NEXT_PUBLIC_BASE_URL+'assets/images/anjul-gupta.png?auto=format&fit=crop&w=500&q=60'}
+                      src={
+                        process.env.NEXT_PUBLIC_BASE_URL +
+                        "assets/images/anjul-gupta.png?auto=format&fit=crop&w=500&q=60"
+                      }
                       alt="image"
                     />
                     <ul className="social">
@@ -162,8 +189,13 @@ const About = () => {
                     <h3>Anjul Gupta</h3>
                     <span>CFA (Co-Founder and CEO)</span>
                   </div>
-                  <div className="content" style={{"textAlign":"left"}}>
-                    <p>Anjul is a CFA charterholder and an MBA in Finance. He specializes in Business Valuation and an alumnus of Deloitte and Duff & Phelps. Currently, he oversees the business’s overall operations and strategy.</p>
+                  <div className="content" style={{ textAlign: "left" }}>
+                    <p>
+                      Anjul is a CFA charterholder and an MBA in Finance. He
+                      specializes in Business Valuation and an alumnus of
+                      Deloitte and Duff & Phelps. Currently, he oversees the
+                      business’s overall operations and strategy.
+                    </p>
                   </div>
                 </div>
               </div>
@@ -175,7 +207,10 @@ const About = () => {
                       alt="image"
                     /> */}
                     <img
-                      src={process.env.NEXT_PUBLIC_BASE_URL+'assets/images/megha-agarwal.png?auto=format&fit=crop&w=500&q=60'}
+                      src={
+                        process.env.NEXT_PUBLIC_BASE_URL +
+                        "assets/images/megha-agarwal.png?auto=format&fit=crop&w=500&q=60"
+                      }
                       alt="image"
                     />
                     <ul className="social">
@@ -205,8 +240,14 @@ const About = () => {
                     <h3>CA Megha Agarwal</h3>
                     <span>(Co-Founder and CFO)</span>
                   </div>
-                  <div className="content" style={{"textAlign":"left"}}>
-                    <p>CA Megha Agarwal is Fellow Member of Institute of Chartered Accountants of India. She has 19 years of experience of CA Practice in the area of Direct & Indirect Taxation and Auditing.  She oversees the finance & operations functions in the Company.</p>
+                  <div className="content" style={{ textAlign: "left" }}>
+                    <p>
+                      CA Megha Agarwal is Fellow Member of Institute of
+                      Chartered Accountants of India. She has 19 years of
+                      experience of CA Practice in the area of Direct & Indirect
+                      Taxation and Auditing. She oversees the finance &
+                      operations functions in the Company.
+                    </p>
                   </div>
                 </div>
               </div>
@@ -250,19 +291,44 @@ const About = () => {
           </div>
           <div className="default-shape">
             <div className="shape-1">
-            <Image src="assets/img/shape/4.png" alt="image" width={15} height={15}/>
+              <Image
+                src="assets/img/shape/4.png"
+                alt="image"
+                width={15}
+                height={15}
+              />
             </div>
             <div className="shape-2 rotateme">
-            <Image src="assets/img/shape/5.svg" alt="image" width={22} height={22}/>
+              <Image
+                src="assets/img/shape/5.svg"
+                alt="image"
+                width={22}
+                height={22}
+              />
             </div>
             <div className="shape-3">
-            <Image src="assets/img/shape/6.svg" alt="image" width={21} height={20}/>
+              <Image
+                src="assets/img/shape/6.svg"
+                alt="image"
+                width={21}
+                height={20}
+              />
             </div>
             <div className="shape-4">
-            <Image src="assets/img/shape/7.png" alt="image" width={18} height={18}/>
+              <Image
+                src="assets/img/shape/7.png"
+                alt="image"
+                width={18}
+                height={18}
+              />
             </div>
             <div className="shape-5">
-            <Image src="assets/img/shape/8.png" alt="image" width={12} height={12}/>
+              <Image
+                src="assets/img/shape/8.png"
+                alt="image"
+                width={12}
+                height={12}
+              />
             </div>
           </div>
         </section>
