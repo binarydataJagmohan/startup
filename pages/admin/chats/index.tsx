1e1b96af63255ff45a228c8534e24483b8886e95
binarydata123 <binarydata.sale@gmail.com>
2023-11-22 12:13:14 +0530

    investor, admin and startup chat code changes upload
    investor, admin and startup chat code changes upload

---
+++ pages/admin/chats/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AdminChats from '../../../components/Admin/Chats/Chats';
+export default function Chats() {
+  return (
+        <>
+            <AdminChats/>
+        </>
+  )
+}
